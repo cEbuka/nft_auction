a232910aeda196ca13f53ad7470f42fad8ad10f5
cEbuka <kastecebuka20@gmail.com>
2025-02-08 00:00:55 +0100

    set max supply

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -7,11 +7,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // ERC721 NFT Contract
 contract NFT is ERC721URIStorage, Ownable {
     uint256 private _tokenIdCounter;
+    uint256 public constant MAX_SUPPLY = 10;
+    
 
     constructor() ERC721("Baby Sharks", "BSS") Ownable(msg.sender) {}
 
     // Mint function to create new NFTs
     function safeMint(address to, string memory tokenURI) external onlyOwner {
+        require(_tokenIdCounter < MAX_SUPPLY, "Max supply reached");
         uint256 tokenId = _tokenIdCounter;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, tokenURI);
