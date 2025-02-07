3e53f851916ea3ca777f8fb3e9724ecfa9200f0c
cEbuka <kastecebuka20@gmail.com>
2025-02-07 23:39:00 +0100

    added uri

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -1,30 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/draft/Counter.sol";
 
 // ERC721 NFT Contract
-contract NFT is ERC721Full, Ownable {
-    using Counters for  Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
+contract NFT is ERC721URIStorage, Ownable {
+    uint256 private _tokenIdCounter;
 
-    constructor(
-        string memory name,
-        string memory symbol
-    ) ERC721Full(name, symbol) Ownable(msg.sender) {}
+    constructor() ERC721("Baby Sharks", "BSS") Ownable(msg.sender) {}
 
     // Mint function to create new NFTs
     function safeMint(address to, string memory tokenURI) external onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
+        uint256 tokenId = _tokenIdCounter;
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, tokenURI)
-        _tokenIdCounter.increment();
-    }
-
-    // Function to get the current token ID count
-    function getCurrentTokenId() external view returns (uint256) {
-        return tokenIdCounter;
+        _setTokenURI(tokenId, tokenURI);
+        _tokenIdCounter++;
     }
 }
